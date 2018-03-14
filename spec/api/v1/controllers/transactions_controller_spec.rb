03f75500993a19856b94564fad9ef58f00e242d8
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2018-03-14 15:21:35 -0300

    initial setup

---
+++ spec/api/v1/controllers/transactions_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+Rspec.describe Api::V1::TransactionController, type: :request do
+  describe 'get #show' do
+    it 'returns a list of transactions' do
+      get '/api/v1/survivors/1'
+      json = JSON.parse(last_response.body)
+      expect(json["data"]["id"]).to eq(1)
+    end
+  end
+end
+
